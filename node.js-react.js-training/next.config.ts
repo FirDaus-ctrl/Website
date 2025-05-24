40fbf5d97c0a2f55eef4f755b0bbbdfe6eb4ec0d
FirDaus-ctrl <fdaus8080@gmail.com>
2025-05-24 03:44:46 +0000

    added "https://21st.dev/r/aceternity/timeline" and added it to the about page

--- node.js-react.js-training/next.config.ts
+++ node.js-react.js-training/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.aceternity.com",
+      },
+      {
+        protocol: "https",
+        hostname: "conceptstech.com.bn",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
