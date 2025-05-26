3e22b8357f3677e68b9448aec2b35073de251270
FirDaus-ctrl <fdaus8080@gmail.com>
2025-05-26 03:31:20 +0000

    fixing the git and everything from previous commit

--- node.js-react.js-training/.next/server/server-reference-manifest.js
+++ node.js-react.js-training/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yamhLdfgk8OjHuxyn3i2U/XmXjXkA5HJpH+1IZhZTB8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dEvWCrH3Mb/mugRvhwAJpEB0ZSPk4S+Nylwdzyj7QSM=\"\n}"
